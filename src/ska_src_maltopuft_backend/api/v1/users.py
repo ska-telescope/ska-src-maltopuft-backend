144b203cfa41f7517479c144d717345cc9ac3d4f
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-03-27 09:59:19 +0000

    TEAL-538: Apply lower line-length (from 88 to 77)

--- src/ska_src_maltopuft_backend/api/v1/users.py
+++ src/ska_src_maltopuft_backend/api/v1/users.py
@@ -12,6 +12,8 @@ user_router = APIRouter()
 
 @user_router.post("/")
 async def post_user(user: User) -> dict:
-    """Return the information POSTed by a user back to the client for testing."""
+    """Return the information POSTed by a user back to the client for
+    testing.
+    """
     logger.debug("This is an example log statement.")
     return {"name": f"{user.name}"}
