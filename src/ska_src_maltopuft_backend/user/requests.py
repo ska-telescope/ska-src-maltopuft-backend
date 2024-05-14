8a44efcdb9463a3e1ec943a338d883980dae74a2
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-05-14 16:41:56 +0100

    DAAC-91: Implement user service

---
+++ src/ska_src_maltopuft_backend/user/requests.py
@@ -0,0 +1,40 @@
+"""User request schemas."""
+
+from typing import Annotated
+from uuid import uuid4
+
+from fastapi import Query
+from pydantic import UUID4, BaseModel, EmailStr, Field, StringConstraints
+
+
+class GetUser(BaseModel):
+    """Attributes for User model GET requests."""
+
+    id: int | None = Field(default=None, gt=0)
+    uuid: UUID4 | None = None
+    email: EmailStr | None = None
+    username: (
+        Annotated[str, StringConstraints(strip_whitespace=True)] | None
+    ) = None
+    is_admin: bool | None = None
+
+
+class CreateUser(BaseModel):
+    """Attributes for User model POST requests."""
+
+    uuid: UUID4 = Field(default_factory=uuid4)
+    email: EmailStr
+    username: Annotated[str, StringConstraints(strip_whitespace=True)]
+    is_admin: bool = False
+
+
+class UserQueryParams(BaseModel):
+    """Query parameters for User model GET requests."""
+
+    id: list[Annotated[int, None]] = Field(Query(default=[]))
+    uuid: list[Annotated[UUID4, None]] = Field(Query(default=[]))
+    email: list[Annotated[EmailStr, None]] = Field(Query(default=[]))
+    username: list[
+        (Annotated[str, StringConstraints(strip_whitespace=True)] | None)
+    ] = Field(Query(default=[]))
+    is_admin: bool | None = Field(Query(default=None))
--- src/ska_src_maltopuft_backend/user/router.py
+++ src/ska_src_maltopuft_backend/user/router.py
@@ -1,20 +1,47 @@
 """Routers for user endpoints."""
 
 import logging
+from typing import Any
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from src.ska_src_maltopuft_backend.core.auth import Authenticated
+from src.ska_src_maltopuft_backend.core.database import get_db
+from src.ska_src_maltopuft_backend.user.controller import user_controller
+from src.ska_src_maltopuft_backend.user.requests import (
+    CreateUser,
+    UserQueryParams,
+)
 from src.ska_src_maltopuft_backend.user.responses import User
 
 logger = logging.getLogger(__name__)
 user_router = APIRouter()
 
 
-@user_router.post("/", dependencies=[Depends(Authenticated)])
-async def post_user(user: User) -> dict:
-    """Return the information POSTed by a user back to the client for
-    testing.
-    """
+@user_router.get("/", response_model=list[User])
+async def get_users(
+    q: UserQueryParams = Depends(),
+    db: Session = Depends(get_db),
+) -> Any:
+    """Get all users."""
     logger.debug("This is an example log statement.")
-    return {"name": f"{user.name}"}
+    return await user_controller.get_all(db=db, q=q)
+
+
+@user_router.get("/{user_id}", response_model=User)
+async def get_user(user_id: int, db: Session = Depends(get_db)) -> User:
+    """Get user by id."""
+    return await user_controller.get_by_id(db=db, id_=user_id)
+
+
+@user_router.post("/")
+async def post_user(user: CreateUser, db: Session = Depends(get_db)) -> User:
+    """Create a new user."""
+    return await user_controller.create(db=db, attributes=user.model_dump())
+
+
+@user_router.delete("/{user_id}")
+async def delete_user(user_id: int, db: Session = Depends(get_db)) -> None:
+    """Delete user by id."""
+    return await user_controller.delete(db=db, id_=user_id)
