e0357759f566816af3edbcdde0713e4382c10f9c
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-05 17:39:41 +0100

    TEAL-505: Define CoherentBeamConfig table

--- src/ska_src_maltopuft_backend/observation/models.py
+++ src/ska_src_maltopuft_backend/observation/models.py
@@ -1,9 +1,10 @@
 """Observation metadata models."""
 
 import datetime as dt
+from decimal import Decimal
 
 import sqlalchemy as sa
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ska_src_maltopuft_backend.core.database.base import Base
 from ska_src_maltopuft_backend.core.mixins import TimestampMixin
@@ -17,6 +18,14 @@ class ScheduleBlock(Base, TimestampMixin):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     start_at: Mapped[dt.datetime] = mapped_column(nullable=False)
     est_end_at: Mapped[dt.datetime] = mapped_column(nullable=False)
+
+    # Relationships
+    meerkat_schedule_block: Mapped["MeerkatScheduleBlock"] = relationship(
+        back_populates="schedule_block",
+    )
+    observations: Mapped[list["Observation"]] = relationship(
+        back_populates="schedule_block",
+    )
 
     __table_args__ = (sa.UniqueConstraint("start_at", "est_end_at"),)
 
@@ -46,6 +55,11 @@ class MeerkatScheduleBlock(Base, TimestampMixin):
         nullable=False,
     )
 
+    # Relationships
+    schedule_block: Mapped["ScheduleBlock"] = relationship(
+        back_populates="meerkat_schedule_block",
+    )
+
     __table_args__ = (
         sa.UniqueConstraint("meerkat_id"),
         sa.UniqueConstraint("meerkat_id_code"),
@@ -61,3 +75,31 @@ class MeerkatScheduleBlock(Base, TimestampMixin):
             f"proposal_id={self.proposal_id},"
             f"schedule_block_id={self.schedule_block_id},"
         )
+
+
+class CoherentBeamConfig(Base, TimestampMixin):
+    """Meerkat telescope observation coherent beam configuration."""
+
+    __tablename__ = "coherent_beam_config"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    angle: Mapped[float] = mapped_column(nullable=False)
+    fraction_overlap: Mapped[Decimal] = mapped_column(nullable=False)
+    x: Mapped[float] = mapped_column(nullable=False)
+    y: Mapped[float] = mapped_column(nullable=False)
+
+    __table_args__ = (
+        sa.UniqueConstraint("angle", "fraction_overlap", "x", "y"),
+    )
+
+    def __repr__(self) -> str:
+        """CoherentBeamConfig repr."""
+        return (
+            "<CoherentBeamConfig: "
+            f"id={self.id},"
+            f"angle={self.angle},"
+            f"fraction_overlap={self.fraction_overlap},"
+            f"x={self.x},"
+            f"y={self.y},"
+        )
+
