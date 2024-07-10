ff1b1b0748fedff9eb8059558279221491232c5b
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-10 08:45:24 +0100

    TEAL-505: Export observation models in app/models/__init__.py

--- src/ska_src_maltopuft_backend/app/models/__init__.py
+++ src/ska_src_maltopuft_backend/app/models/__init__.py
@@ -4,8 +4,13 @@ from ska_src_maltopuft_backend.candle.models import Candidate, SPCandidate
 from ska_src_maltopuft_backend.core.database.base import Base
 from ska_src_maltopuft_backend.label.models import Entity, Label
 from ska_src_maltopuft_backend.observation.models import (
+    Beam,
+    CoherentBeamConfig,
+    Host,
     MeerkatScheduleBlock,
+    Observation,
     ScheduleBlock,
+    TilingConfig,
 )
 from ska_src_maltopuft_backend.user.models import User
 
@@ -18,4 +23,9 @@ __all__ = [
     "Label",
     "ScheduleBlock",
     "MeerkatScheduleBlock",
+    "CoherentBeamConfig",
+    "Observation",
+    "TilingConfig",
+    "Beam",
+    "Host",
 ]
