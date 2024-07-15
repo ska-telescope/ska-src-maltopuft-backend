c2f5259a4551fed7a977e6392534b468c6bb63f3
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-07-15 15:07:12 +0100

    DAAC-157: Revert commit f22d5b3 (Add UNLABELLED entity)

--- src/ska_src_maltopuft_backend/core/extras.py
+++ src/ska_src_maltopuft_backend/core/extras.py
@@ -4,5 +4,11 @@ from typing import Annotated, TypeVar
 
 from annotated_types import Gt
 
+from ska_src_maltopuft_backend.core.database.base import Base
+
 T = TypeVar("T")
 PositiveList = list[Annotated[T, Gt(0)]]
+
+ModelT = TypeVar("ModelT", bound=Base)
+CreateModelT = TypeVar("CreateModelT")
+UpdateModelT = TypeVar("UpdateModelT")
